dc199c8ca52e323a257750a72dc8cf41a9d91c8c
Jeremy Weed <jweed262@gmail.com>
2019-04-14 00:57:25 -0400

    testing

--- test.py
+++ test.py
@@ -2,20 +2,28 @@ import argparse
 import torch
 from flamebot import RNN
 from torch.autograd import Variable
+from collections import deque
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="model tester")
     parser.add_argument('starting_char', type=str, help='Seed character')
     parser.add_argument('model_file', type=str, help='PyTorch model to load',
-                        default='flamebot.pt')
-    parser.add_argument('string_length', type=int, help='Length of string to'
-                        + 'generate')
+                        default='flamebot2000.pt')
 
     args = parser.parse_args()
-    model = RNN()
+    model = RNN(40, 40, 128, 128)
     model.load_state_dict(torch.load(args.model_file))
-    seed = args.starting_char
-    out = str(model(ord(seed)))
+    seed = args.starting_char * 40
+
+    def string_to_byte(string):
+        return [ord(s) for s in string]
+
+    def byte_to_string(byte):
+        return [str(byte) for b in byte]
+
+    out = deque(string_to_byte(seed), 40)
     for i in range(args.string_length):
-        out += str(model(out[-1]))
+        out.append(str(model(list(out))))
+
+    print("generated:" + byte_to_string(list(out)))
