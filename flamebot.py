26fabdfeddda4fa5c54da10a9edb3a604d6d51d8
Jeremy Weed <jweed262@gmail.com>
2019-04-13 22:15:52 -0400

    adsf

--- flamebot.py
+++ flamebot.py
@@ -41,7 +41,7 @@ class RNN(nn.Module):
         l2, _ = self.lstm2(l1)
         attn_weights = F.softmax(self.attn(torch.cat((embeddings, l1, l2), dim=2)),
                                  dim=0)
-        _, output = torch.max(self.softmax(self.output(attn_weights)))
+        _, output = torch.max(self.softmax(self.output(attn_weights)).unsqueeze(dim=0))
         return output
 
     def initHidden(self):
