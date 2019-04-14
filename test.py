e7362c3c15d7525576368eb28b78db93522ab053
Jeremy Weed <jweed262@gmail.com>
2019-04-14 01:54:52 -0400

    giving up

--- test.py
+++ test.py
@@ -14,19 +14,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
     model = RNN(128, 40, 128, 128)
     model.load_state_dict(torch.load(args.model_file))
-    seed = args.starting_char * 40
+    seed = 'abcdefghijklmnopqrstabcdefghijklmnopqrst'
+    # seed = 'a' * 40
+    print(seed)
 
     def string_to_byte(string):
         return [ord(s) for s in string]
 
     def byte_to_string(byte):
-        return [str(byte) for b in byte]
+        return [str(chr(int(b))) for b in byte]
+
+    def blist(bs):
+        return [byte_to_string(b) for b in bs]
 
     out = deque(string_to_byte(seed), 40)
     for i in range(40):
         d = model(torch.tensor(list(out), dtype=torch.long)
-                  .unsqueeze(0))
-        print(d)
-        out.append(d.tolist())
+                  .unsqueeze(dim=0))
+        print(d.size())
+        print(blist(d.tolist()))
+        out.append(d.tolist()[0])
+        # print(byte_to_string(list(out)))
 
     print("generated:" + byte_to_string(out.tolist()))
